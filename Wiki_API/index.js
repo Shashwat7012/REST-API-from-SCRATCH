e5e7884fea36e74038a126530b71443b91d8ddc4
Shashwat Pandey <105585782+Shashwat7012@users.noreply.github.com>
2023-08-10 17:14:41 +0530

    Update index.js

--- Wiki_API/index.js
+++ Wiki_API/index.js
@@ -19,41 +19,128 @@ const articleSchema = {
     content: String
 };
 const Article = mongoose.model("Article",articleSchema)
-
-
-app.get("/articles", async (req,res)=>{
+//chained method
+app.route("/articles").get(async (req,res)=>{
    
-        try{
-            const foundArticles = await Article.find();
-            console.log(foundArticles);
-            res.send(foundArticles)
-        }
-        catch{
-            res.send("There is an error")
-        }
-        
-    });
-    app.post("/articles", async (req,res)=>{
-        try{
-           console.log(req.body.title);
-            console.log(req.body.content);
-            //Dont have any html,ejs,form data.So,post this data to postman and enter key value.
-            //Then send the data from postman
-            const newArrticle =  new Article({
+    try{
+        const foundArticles = await Article.find();
+        console.log(foundArticles);
+        res.send(foundArticles)
+    }
+    catch{
+        res.send("There is an error")
+    }
+    
+})
+
+app.route("/articles")
+    .post(async (req, res) => {
+        try {
+            const newArticle = new Article({
                 title: req.body.title,
                 content: req.body.content
             });
-          await newArrticle.save()
-            res.send("Seccessfully saved")//display the message to the sender.
+            await newArticle.save();
+            res.send("Successfully saved");
+        } catch (error) {
+            console.error("Error saving article:", error);
+            res.status(500).send("Error saving article");
+        }
+    })
+app.route("/articles")
+    .post(async (req, res) => {
+        try {
+            const newArticle = new Article({
+                title: req.body.title,
+                content: req.body.content
+            });
+            await newArticle.save();
+            console.log(newArticle.save());
+            res.send("Successfully saved");
+           
+        } catch (error) {
+            console.error("Error saving article:", error);
+            res.status(500).send("Error saving article");
+        }
+    })
+
+.delete(async (req, res) => {
+    try {
+        const result = await Article.deleteMany({});
+        try{
+            await res.send("Successfully Deleted");
+        } catch {
+            res.send("No articles found to delete");
+        }
+    } catch  {
+        res.send("error");
+    }
+})
+//bcz of chained rule we will not use app.get or post or anything. we will use app.route.
+app.route("/articles/:articleTitle")
+
+.get(async (req,res)=>{
+
+    try{
+       const foundArticle =  await Article.findOne({title:req.params.articleTitle});
+        try{
+            await res.send(foundArticle);
         }
         catch{
-            console.log("Error");
+            res.send("error");
         }
-    });
+    }
+    catch{
+        res.send("Found Error");
+    }
+})
 
+.put(async (req, res) => {
+    try {
+        await Article.updateOne(
+            { title: req.params.articleTitle },
+          {$set:  { title: req.body.title, content: req.body.content }}, 
+            { overwrite: true }
+        );
+        res.send("Successfully updated");
+    } catch{
+        // console.error("Error updating article:", error);
+        res.send("Error updating article");
+    }
+})
+// we can update a single thing in PUT.
+.patch( async (req,res)=>{
+            try {
+                await Article.updateOne(
+                    { title: req.params.articleTitle },
+                  {$set:  { title: req.body.title, content: req.body.content }}, 
+                    { overwrite: true }
+                );
+                res.send("Successfully updated");
+            } catch{
+            res.send("error");
+        }
+})
+.delete(async (req, res) => {
+    try {
+        await Article.deleteOne({ title: req.params.articleTitle });
+        res.send("Successfully Deleted");
+    } catch (error) {
+        res.status(500).send("Error deleting article");
+    }
+});
 
+//get
+// app.get("/articles", );
+    //post
+    // app.post("/articles", );
 
-
+    //delete
+    // app.delete("/articles", );
+    
+    
 app.listen(3000,()=>{
     console.log("Server Started On Port 3000")
 })
+
+//if we have space between the title then instead of space use %20 bcz value of space in UTF-8 :- %20
